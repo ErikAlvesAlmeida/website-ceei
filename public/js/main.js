7572dfd7f43535cf39982c546512fa8cfe1a1853
Erik <eriikalves55@gmail.com>
2025-09-27 01:10:38 -0300

    feat: initial landing-page

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const navbar = document.querySelector('.navbar');
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+});
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
