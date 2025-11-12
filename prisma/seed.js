4174266385d5273bd566ce17ab1d5423c760e01e
Erik <eriikalves55@gmail.com>
2025-11-12 17:55:25 -0300

    UPDATE: seed.js && configurações do docker-compose

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,4 +1,6 @@
-require('dotenv').config(); 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config(); 
+}
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 
